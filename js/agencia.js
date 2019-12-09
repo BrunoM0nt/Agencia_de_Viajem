0b04a189391d2f684150b8aa9d87bf047a8df121
BrunoM0nt <56006427+BrunoM0nt@users.noreply.github.com>
2019-12-09 20:12:21 -0300

    criação da tela menu,cadastro e exibir

---
+++ js/agencia.js
@@ -0,0 +1,13 @@
+app.initialize();
+
+function abrir_tela_cadastro() {
+    $("#menu").hide();//escode tela inicial
+    $("#tela_cadastro").show(); // mostra tela de edicao
+
+}
+
+function abrir_tela_lista() {
+    $("#menu").hide();//escode tela inicial
+    $("#tela_lista").show(); // mostra tela de edicao
+
+}
