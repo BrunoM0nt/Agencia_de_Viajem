2a04f051d25e5debc29df45f2f1e08cf47918f10
BrunoM0nt <56006427+BrunoM0nt@users.noreply.github.com>
2019-12-09 20:42:06 -0300

    criação e inserção no BD

--- js/agencia.js
+++ js/agencia.js
@@ -1,13 +1,43 @@
 app.initialize();
 
+var db = window.openDatabase("Database", "1.0", "Agenda", 2000);
+db.transaction(createDB, errorDB, successDB);
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    db.transaction(createDB, errorDB, successDB);
+}
+
+function errorDB(err) {
+    alert("Erro: " + err);
+}
+
+function successDB() { }
+
+function createDB(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Agencia(id INTEGER PRIMARY KEY, nome VARCHAR(50), qtd_estoque NUM(15),preco float(15))');
+}
+
+function agencia_insert() {
+    db.transaction(agencia_insert_db, errorDB, successDB)
+}
+
+function agencia_insert_db(tx) {
+    var nome = $("#nome_pacote").val();
+    var qtd_estoque = $("#quantidade_estoque").val();
+    var preco = $("#pacote_preco").val();
+    tx.executeSql('INSERT INTO Agencia (nome,qtd_estoque,preco) VALUES ("' + nome + '","' + qtd_estoque + '","' + preco + '")');
+}
+
+
 function abrir_tela_cadastro() {
-    $("#menu").hide();//escode tela inicial
-    $("#tela_cadastro").show(); // mostra tela de edicao
+    $("#menu").hide();//escode tela menu
+    $("#tela_cadastro").show(); // mostra tela de cadastro
 
 }
 
 function abrir_tela_lista() {
-    $("#menu").hide();//escode tela inicial
-    $("#tela_lista").show(); // mostra tela de edicao
+    $("#menu").hide();//escode tela menu
+    $("#tela_lista").show(); // mostra tela lista
 
 }
