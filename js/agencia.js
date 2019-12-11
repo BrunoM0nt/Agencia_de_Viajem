57ae80f608827fcb7cd2dce5c0870ee04f32ea49
Rgnwld <regifilho06@gmail.com>
2019-12-11 17:33:55 -0300

    pela metade db

--- js/agencia.js
+++ js/agencia.js
@@ -15,8 +15,8 @@ function errorDB(err) {
 function successDB() { }
 
 function createDB(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS Produtos(id_Produto INTEGER PRIMARY KEY, nome VARCHAR(50) , qtd_estoque NUM(15),preco NUM(15))');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS Carrinho(id_Compra INTEGER PRIMARY KEY, nome VARCHAR(50), qtd NUM(15),total NUM(15))');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Agencia(id INTEGER PRIMARY KEY, nome VARCHAR(50) , qtd_estoque NUM(15),preco NUM(15))');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Carrinho(id_Compra INTEGER PRIMARY KEY, id_prod NUM(15), qtd NUM(15))');
 }
 
 function agencia_insert() {
@@ -41,6 +41,7 @@ function agencia_view_db(tx) {
     tx.executeSql('SELECT * FROM Agencia', [], agencia_view_data, errorDB);
 }
 
+//Apresentação de dados ------------------+
 function agencia_view_data(tx, results) {
     $("#pacotes_listagem").empty();
     var len = results.rows.length;
@@ -50,25 +51,25 @@ function agencia_view_data(tx, results) {
             "<td> <h3>" + results.rows.item(i).nome + "</h3> </td >" +
             "<td> <h3>" + results.rows.item(i).qtd_estoque + "</h3> </td >" +
             "<td> <h3>" + results.rows.item(i).preco + "</h3> </td >" +
-            "<td><input type='button' class='btn btn-lg btn-warning' value='Comprar' onclick='view_compra(" + results.rows.item(i).id + ")'>" +
+            "<td><input type='button' class='btn btn-lg btn-warning' value='Comprar' onclick='compra_tela(" + results.rows.item(i).id + ")'>" +
             "<td><input type='button' class='btn btn-lg btn-danger' value='x' + onclick='agenda_delete(" + results.rows.item(i).id + ")'>" +
-            //"<input type='button'  class='btn btn-lg btn-warning' value='Add'  onclick='agenda_update_abrir_tela(" + results.rows.item(i).id + ")'></td>" +
             "</tr>");
     }
 }
 // Consulta no banco de dados o item selecionado na compra
-function view_compra(compra_id) {
-    $("#agencia_id_compra").val(compra_id);
+function compra_tela(compra_id) {
+    $("#id_compra").val(compra_id);
     db.transaction(compra_view_db, errorDB, successDB)
 }
-function compra_view_db(tx) {
-    var agencia_id_compra = $("#agencia_id_compra").val();
+function compra_tela_db(tx) {
+    var id_compra = $("#id_compra").val();
     $("#tela_lista").hide();
     $("#tela_compra").show();
-    tx.executeSql('SELECT * FROM Agencia where id = ' + agencia_id_compra + '', [], agencia_view_compra, errorDB);
+    tx.executeSql('SELECT * FROM Agencia where id = ' + id_compra + '', [], compra_tela_data, errorDB);
 }
 
-function agencia_view_compra(tx, results) {
+//Compras ------------------+
+function compra_tela_data(tx, results) {
     $("#listagem_compra").empty();
     var len = results.rows.length;
     for (var i = 0; i < len; i++) {
@@ -77,22 +78,73 @@ function agencia_view_compra(tx, results) {
             "<td> <h3>" + results.rows.item(i).qtd_estoque + "</h3> </td >" +
             "<td> <h3>" + results.rows.item(i).preco + "</h3> </td >" +
             "<td><br><input id='qtd_compra' type='text' class='form-control'>" +
-            "<td><br><input type='button' class='btn btn-lg btn-warning' value='Confirmar Compra' onclick='view_compra(" + results.rows.item(i).id + ")'>" +
+            "<td><br><input type='button' class='btn btn-lg btn-warning' value='Continuar Compra' onclick='continuar_compra(" + results.rows.item(i).id + ", " + $('qtd_compra').val() +")'>" +
             "</tr>");
     }
 }
 
+function continuar_compra(prod_id, prod_qtd){
+    $("#prod_id").val(prod_id);
+    $("#prod_qtd").val(prod_qtd);
+    alert(prod_qtd)
+    db.transaction(continuar_compra_db, errorDB, successDB)
+}
+
+function continuar_compra_db(tx){
+    tx.executeSql("INSERT INTO carrinho (id_prod, qtd) VALUES('" + $("#prod_id").val() + "', '" + $("#prod_qtd").val() + "')")
+    alert($("#prod_id").val())
+    alert($("#prod_qtd").val())
+}
+
+function compra_view() {
+    db.transaction(compra_view_db, errorDB, successDB)
+}
+
+function compra_view_db(tx) {
+    tx.executeSql('SELECT * FROM produtos', [], compra_view_data, errorDB);
+}
+
+//Apresentação de dados ------------------+
+function compra_view_data(tx, results) {
+    $("#pacote_compra").empty();
+    var len = results.rows.length;
+
+    for (var i = 0; i < len; i++) {
+        $("#pacote_compra").append("<tr class='compra_item_compra'>" +
+            "<td> <h3>" + results.rows.item(i).nome + "</h3> </td >" +
+            "<td> <h3>" + results.rows.item(i).qtd_estoque + "</h3> </td >" +
+            "<td> <h3>" + results.rows.item(i).preco + "</h3> </td >" +
+            "<td><input type='button' class='btn btn-lg btn-danger' value='x' + onclick='compra_item_delete(" + results.rows.item(i).id + ")'>" +
+            //"<input type='button'  class='btn btn-lg btn-warning' value='Add'  onclick='agenda_update_abrir_tela(" + results.rows.item(i).id + ")'></td>" +
+            "</tr>");
+    }
+}
+
+function compra_item_delete(agencia_id) {
+    document.getElementById('id_delete').value = agencia_id;
+    db.transaction(compra_item_delete_db, errorDB, successDB)
+}
+
+function compra_item_delete_db(tx) {
+    alert("Item removido da lista!")
+    tx.executeSql("delete from produtos where id = '" + document.getElementById('id_delete').value + "'");
+    agencia_view();
+}
+
 function agenda_delete(agencia_id) {
-    document.getElementById('agenda_id_delete').value = agencia_id;
+    document.getElementById('id_delete').value = agencia_id;
     db.transaction(agenda_delete_db, errorDB, successDB)
 }
 
 function agenda_delete_db(tx) {
     alert("Item removido da lista!")
-    tx.executeSql("delete from Agencia where id = '" + document.getElementById('agenda_id_delete').value + "'");
+    tx.executeSql("delete from Agencia where id = '" + document.getElementById('id_delete').value + "'");
     agencia_view();
 }
 
+
+
+0
 function limpar() {
     if (document.getElementById('nome_pacote').value != "" || document.getElementById('quantidade_estoque').value != "" || document.getElementById('pacote_preco').value != "") {
         document.getElementById('nome_pacote').value = "";
